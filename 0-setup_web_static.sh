c1c0f79ab49b9cf5202fd021eb1cee0f32cbf687
bollo-omar <bolloaggrey@gmail.com>
2023-04-07 01:20:26 +0000

    initila commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,8 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<html><head></head><body><h2>This is a test</h2></body></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR root:root /data/
+sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx start
