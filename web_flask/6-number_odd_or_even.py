dca9a66d7f8c3013a2e58693036ec854f29f631b
bollo-omar <bolloaggrey@gmail.com>
2023-05-19 08:45:17 +0000

    task 03

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,73 +1,55 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-from flask import Flask
-from flask import render_template
+""" Script that starts a Flask web application """
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """
-    Displays 'Hello HBNB!'
-    """
-    return "Hello HBNB!"
+    """ Print Web """
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb_display():
-    """
-    displays "HBNB"
-    """
-    return "HBNB"
+@app.route('/hbnb')
+def hbnb():
+    """ Print Web """
+    return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def cisfun(text):
-    """
-    Displays 'C' followed by the value of text
-    """
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """ Print a char C followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """
-    Displays 'Python' followed by the expansion of <text>
-    """
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
+@app.route('/python')
+@app.route('/python/<text>')
+def python_is_cool(text='is cool'):
+    """ Print Python, followed by the value of the text variable,
+    with default value of text: is cool """
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/number/<int:n>')
 def number(n):
-    """
-    Displays 'n is a number' only if n is an integer.
-    """
-    return "{} is a number".format(n)
+    """ Number route """
+    return '{} is number'.format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def number_template(n):
-    """
-    Displays an HTML page only if <n> is an integer.
-    """
-    return render_template("5-number.html", n=n)
+    """ Display a HTML page only if n is an integer """
+    return render_template('5-number.html', number=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
-    """
-    Displays an HTML page only if <n> is an integer .
-    Displays whether <n> is even or odd .
-    """
-    return render_template("6-number_odd_or_even.html", n=n)
+    """ Show if the number is even or odd """
+    return render_template('6-number_odd_or_even.html', n=n)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000)
